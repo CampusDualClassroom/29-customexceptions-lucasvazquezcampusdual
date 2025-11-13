e0d5ccf59f8426e905735754dc370d6481c3a4a2
Lucas Vázquez <lucasvazquezarias@gmail.com>
2025-11-13 16:57:07 +0100

    Exercise 29

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,11 +2,23 @@ package com.campusdual.classroom;
 
 public class Exercise {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
+    public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException {
+        try {
+            return dividend / divisor;
+        } catch (ArithmeticException e) {
+            throw new DivisionByZeroException("División entre o, error");
+        }
 
     }
 
     public static void main(String[] args) {
-
+        try {
+            int result = Exercise.divisionWithCustomException(3, 0);
+            System.out.println("resultado" + result);
+        } catch (DivisionByZeroException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("finalizando programa");
+        }
     }
 }
